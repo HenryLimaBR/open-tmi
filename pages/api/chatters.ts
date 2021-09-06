d169eec71414f8a1fad0c7df0e3d464781dc26e6
HenryLimaBR <djfilipebr@gmail.com>
2021-09-06 01:30:19 -0300

    Partial Work

---
+++ pages/api/chatters.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { TwitchChannelChatters } from '../../typings/api'
+import axios from 'axios'
+
+async function chatters(req: NextApiRequest, res: NextApiResponse<TwitchChannelChatters | {}>) {
+  const username: string = req.query.username as string
+  if (username) {
+    const chatters: TwitchChannelChatters = (await axios.get(`https://tmi.twitch.tv/group/user/${username}/chatters`)).data
+    return res.status(202).json(chatters)
+  }
+  return res.status(400).json({})
+}
+
+export default chatters
